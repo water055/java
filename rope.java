48e3548719566c5b76aa5df47dcfaf562bf2aeb9
water055 <swttservice@163.com>
2018-09-11 17:29:27 +0800

    17-29-27

---
+++ rope.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class rope {
+
+	public static void main(String[] args) {
+		int rop = 3000;
+		for (int i = 1; rop > 5; i++) {
+			rop = rop / 2;
+			System.out.println("day=" + i);
+		}
+	}
+}
