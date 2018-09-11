8b733557c1012fbc47381147fd2c2820e48c622d
water055 <swttservice@163.com>
2018-09-11 17:29:14 +0800

    17-29-14

---
+++ rabbit.java
@@ -0,0 +1,16 @@
+package homework;
+
+public class rabbit {
+
+	public static void main(String[] args) {
+		for (int x = 1; x <= 100; x++) {
+			if (x % 3 == 1 && x % 5 == 3 && x % 7 == 2) {
+				System.out.println(x);
+
+			}
+
+		}
+
+	}
+
+}
